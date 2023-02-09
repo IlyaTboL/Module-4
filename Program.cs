f52413772c6ec39c5c9fc8804afcf6b5482969ce
Илья Корытов <t.b.o.l@mail.ru>
2023-02-09 19:06:50 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿class MainClass
+{
+    static void Main(string[] args)
+    {
+        int A = 7;
+        int B = 3;
+        double X = 4.2;
+        double Y = 4.1;
+
+        bool C = A < B || X > Y; 
+        Console.WriteLine(C);
+    }
+}
